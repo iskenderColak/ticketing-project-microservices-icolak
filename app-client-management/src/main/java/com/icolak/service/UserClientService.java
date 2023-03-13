8b2a7f1cbb9873103a5b7bf9eb0837cee14ade99
iskenderColak <iclk84@gmail.com>
2023-03-13 23:51:42 +0300

    User service POM, runner and application.yml settings and configuration

--- app-client-management/src/main/java/com/icolak/service/UserClientService.java
+++ app-client-management/src/main/java/com/icolak/service/UserClientService.java
@@ -5,7 +5,10 @@ import org.springframework.cloud.openfeign.FeignClient;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 
-@FeignClient(value = "user-service",url = "http://localhost:9090")
+// @FeignClient(value = "user-service",url = "http://localhost:9090")
+
+@FeignClient(value = "user-service") // Hey Eureka I want an instance from user service
+                        // I don't care which instance, any instance from user-service
 public interface UserClientService {
 
     @GetMapping("/api/v1/user/{username}")
