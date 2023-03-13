9ade1a50b22c564cc46d8506da1d03ddcfa7108e
iskenderColak <iclk84@gmail.com>
2023-03-14 00:02:32 +0300

    after eureka, we don't need url, just value is enough.

--- app-client-management/src/main/java/com/icolak/service/UserClientService.java
+++ app-client-management/src/main/java/com/icolak/service/UserClientService.java
@@ -7,7 +7,7 @@ import org.springframework.web.bind.annotation.PathVariable;
 
 // @FeignClient(value = "user-service",url = "http://localhost:9090")
 
-@FeignClient(value = "user-service") // Hey Eureka I want an instance from user service
+@FeignClient(value = "user-service") // Hey Eureka! I want an instance from user service
                         // I don't care which instance, any instance from user-service
 public interface UserClientService {
 
